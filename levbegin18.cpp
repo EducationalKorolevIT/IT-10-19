b03971e9713e6155306e607810cbe9733ec8d799
lakilev <lev.cat.lev@mail.ru>
2020-01-16 17:40:18 +0300

    дз бигин

---
+++ levbegin18.cpp
@@ -0,0 +1,21 @@
+#include<cmath>
+#include<iostream>
+
+using namespace std;
+
+main(){
+int a=0;
+int b=0;
+int c=0;
+int d=0;
+int f=0;
+int g=0;
+cin>> a;
+cin>> b;
+cin>> c;
+d=b-c;
+f=c-a+1;
+g=d*f;
+cout<< g;
+}
+
