20ffa5748421a24036c31ac57daae27270d7ffb3
sashazvereva <sasazvereva15@fmail.com>
2020-01-16 17:09:14 +0300

    Create n1.cpp

---
+++ n1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main(){
+        int a = 0, x = 0;
+        cin >> x;
+        a = pow(3*x,6)-pow(6*x,2)-7;
+        cout << x << endl;
+
+    }
