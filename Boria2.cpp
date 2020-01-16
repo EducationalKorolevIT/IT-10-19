6a5f40c1e60f8633c22029dc8cb39146ffd00fc7
dex22044 <dex22044@yandex.ru>
2020-01-16 17:10:44 +0300

    Решил задачу
    Решил задачу If15

---
+++ Boria2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long a,b,c;
+    cin >> a >> b >> c;
+    if(a>b){
+        long long aa=a,bb=b;
+        a=bb;b=aa;
+    }
+    if(b>c){
+        long long bb=b,cc=c;
+        c=bb;b=cc;
+    }if(a<c){
+        long long aa=a,cc=c;
+        c=aa;a=cc;
+    }
+    cout<<a<<" "<<b;
+}
