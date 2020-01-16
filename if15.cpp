5736ebbfd851963fbe951b57d6875ab2be2015c9
sashazvereva <sasazvereva15@fmail.com>
2020-01-16 17:16:04 +0300

    переделала

--- if15.cpp
+++ if15.cpp
@@ -4,10 +4,9 @@
 using namespace std;
 
 int main(){
-        int a = 0, x = 0;
+        int x = 0;
         cin >> x;
-        a = pow(3*x,6)-pow(6*x,2)-7;
-        a = 3*pow(x,6) - 6*pow(x,2)-7;
+
         cout << x << endl;
 
     }
