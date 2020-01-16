96482baea67c5e185e30ef32583f3307829fefcb
lakilev <lev.cat.lev@mail.ru>
2020-01-16 17:50:28 +0300

    сделал иф

---
+++ if18.cpp
@@ -0,0 +1,21 @@
+#include<cmath>
+#include<iostream>
+
+using namespace std;
+
+main(){
+int a=0, s=0, d=0;
+cin>> a;
+cin>> s;
+cin>> d;
+if(a==s){
+    cout<< "3";
+}
+if(a==d){
+        cout<< "2";
+}
+if(s==d){
+    cout<< "1";
+}
+}
+
