99f8c1f79e78aa0faca21ca1f4a969ab408d0245
sashazvereva <sasazvereva15@fmail.com>
2020-01-16 17:18:02 +0300

    /

--- n1.cpp
+++ n1.cpp
@@ -6,7 +6,6 @@ using namespace std;
 int main(){
         int a = 0, x = 0;
         cin >> x;
-        a = pow(3*x,6)-pow(6*x,2)-7;
         a = 3*pow(x,6) - 6*pow(x,2)-7;
         cout << x << endl;
 
