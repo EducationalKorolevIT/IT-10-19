08b58cfdacf5b32f8b7696baf23bc2bdbe066a65
sashazvereva <sasazvereva15@fmail.com>
2020-01-16 17:42:19 +0300

    Update if15.cpp

--- if15.cpp
+++ if15.cpp
@@ -4,9 +4,24 @@
 using namespace std;
 
 int main(){
-        int x = 0;
-        cin >> x;
-
-        cout << x << endl;
+        int a = 0, b = 0, c = 0;
+        cin >> a >> b >> c ;
+        int s1 = 0,  s2 = 0, s3 = 0, sum = 0;
+        s1 = a + b;
+        s2 = a + c;
+        s3 = b + c;
+        if (s1 > s2 ){
+                if(s1 > s3)
+            sum += s1;
+        }
+         if (s2 > s1 ){
+                if(s2 > s3)
+            sum += s2;
+        }
+         if (s3 > s1 ){
+                if(s3 > s2)
+            sum += s3;
+        }
+        cout << sum << endl;
 
     }
