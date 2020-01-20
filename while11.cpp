8371d7d9f3be67655899f22bcb6d34961dd6c568
sashazvereva <sasazvereva15@gmail.com>
2020-01-20 21:33:37 +0300

    Create while11.cpp

---
+++ while11.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int n = 0, k = 0, i = 0, sum = 0;
+    cin >> n;
+    while(i <= k){
+        k++;
+        sum += k;
+    }
+    cout << k;
+    return 0;
+}
