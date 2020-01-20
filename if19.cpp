6749f00ec521db4915358de742fec8bce930d329
sashazvereva <sasazvereva15@gmail.com>
2020-01-20 20:54:23 +0300

    Create if19.cpp
    Я запуталась в скобочках.

---
+++ if19.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int a, b, c, d;
+    cin >> a,b,c,d;
+    if(a == b)
+    {
+        if(a == c)
+        {
+          cout << "1";
+        }
+          else cout << "2";
+    }
+
+    else if(c == a)
+        {
+          cout << "3";
+        }
+          else cout << "4";
+    }
+
+
