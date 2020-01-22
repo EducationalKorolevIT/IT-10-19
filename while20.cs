107c674dae04ae47f5dac481be3344b86d8a45d3
sashazvereva <sasazvereva15@gmail.com>
2020-01-22 22:53:39 +0300

    Сделала While 20.
    Закинула сюда, потому что мне не виден мой репозиторий.

---
+++ while20.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace while20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            n = Convert.ToInt32(Console.ReadLine());
+            while ((n > 0) & ((n % 10) != 2))
+            {
+                n++;
+                {
+                    n /= 10;
+                    
+                }
+                if((n > 0) & ((n % 10) == 2))
+                {
+                    Console.WriteLine("TRUE");
+                }
+
+                else
+                {
+                    Console.WriteLine("FALSE");
+                }
+            }
+        }
+    }
+}
